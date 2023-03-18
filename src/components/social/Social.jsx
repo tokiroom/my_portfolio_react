f8c04c4ee4a0750308326b6db2b2c015a9f16188
alexander-kotlyar <alexander-kotlyar@uadreams.com>
2023-03-18 22:24:41 +0100

    Add social links

--- src/components/social/Social.jsx
+++ src/components/social/Social.jsx
@@ -4,13 +4,13 @@ import "./social.css"
 export default function Social() {
   return (
       <React.Fragment>
-          <a className="social-link" href="#" target="_blank">
+          <a className="social-link" href="https://www.facebook.com/alexander.kotlyar.52" target="_blank">
               <i className='icon icon-facebook2'></i>
           </a>
-          <a className="social-link" href="#" target="_blank">
+          <a className="social-link" href="https://www.linkedin.com/in/oleksandr-kotliar-77bb92241/" target="_blank">
               <i className='icon icon-linkedin'></i>
           </a>
-          <a className="social-link" href="#" target="_blank">
+          <a className="social-link" href="https://github.com/tokiroom" target="_blank">
               <i className='icon icon-github'></i>
           </a>
       </React.Fragment>
